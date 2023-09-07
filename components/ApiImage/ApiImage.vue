46be0e48657907154990a4d4e4c298ad60a2a95f
djoks <contact.djoks@gmail.com>
2023-09-07 22:48:12 +0000

    feat: create ApiImage component to render images loaded from the server.

---
+++ components/ApiImage/ApiImage.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import type { ApiImageProps } from "./ApiImage.types"
+
+const { uuid } = defineProps<{ uuid: string }>();
+const src = `https://pet-shop.buckhill.com.hr/api/v1/file/${uuid}`;
+
+</script>
+
+<template>
+  <img :src="src" v-bind="$attrs" :alt="uuid" />
+</template>
+
+<style scoped></style>
