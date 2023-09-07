685becb0915c13abb3390917e70efd247a2d7e43
djoks <contact.djoks@gmail.com>
2023-09-07 00:52:40 +0000

    feat: create FooterSection component.

---
+++ components/FooterSection/FooterSection.types.ts
@@ -0,0 +1,3 @@
+export interface FooterSectionProps {
+	title?: string;
+}
---
+++ components/FooterSection/FooterSection.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import type { FooterSectionProps } from "./FooterSection.types"
+
+const props = defineProps<FooterSectionProps>();
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div v-if="props.title" class="text-md font-bold mb-3">{{ props.title }}</div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
