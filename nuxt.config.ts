669a45733ff7f99fb7d3deb7c74e9cbfdc37d2d4
djoks <contact.djoks@gmail.com>
2023-09-07 00:23:37 +0000

    feat: add roboto font.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	app: {
+		head: {
+			link: [
+				{
+					rel: "stylesheet",
+					href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+				},
+			],
+		},
+	},
+	css: ["@/assets/css/main.css"],
 	modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
 });
