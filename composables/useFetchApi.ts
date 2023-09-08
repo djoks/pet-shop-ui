b986dba29e2bb035d1077180f866a743322671dd
djoks <contact.djoks@gmail.com>
2023-09-08 05:13:47 +0000

    feat: implement api composables with types for fetching data.
    - Implment useFetchApi composable for fetching data.
    - Implement usePagination composable for paginating data returned from useFetchApi.
    - Implement types for Pagination and FetchState.

---
+++ composables/useFetchApi.ts
@@ -0,0 +1,37 @@
+import { ref, Ref } from "vue";
+import type { FetchState } from "../types/FetchState";
+import type { PaginationResponse } from "../types/api/pagination/PaginationResponse";
+
+export function useFetchApi<T>(
+	baseUrl: string,
+	queryParams: Record<string, any> = {}
+): FetchState<Ref<T | PaginationResponse<T> | null>> {
+	const loading = ref(true);
+	const data: Ref<T | PaginationResponse<T> | null> = ref(null);
+	const error = ref<Error | null>(null);
+
+	const queryString = new URLSearchParams(queryParams).toString();
+	const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+
+	fetch(url)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((result) => {
+			data.value = result as T | PaginationResponse<T>;
+			loading.value = false;
+		})
+		.catch((err: Error) => {
+			error.value = err;
+			loading.value = false;
+		});
+
+	return {
+		loading,
+		data,
+		error,
+	};
+}
