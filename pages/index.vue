1d1bf089e79287627ec7ae5291acf8f29179b14c
djoks <contact.djoks@gmail.com>
2023-09-08 05:17:32 +0000

    feat: add HomeSlider to main page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex w-1/2 mx-auto flex-col py-10">
+  <div class="flex w-2/3 mx-auto flex-col py-10">
 
-    <SearchInput />
+    <div class="w-2/3 mx-auto mb-5">
+      <SearchInput />
+    </div>
 
+    <HomeSlider />
   </div>
 </template>
 
