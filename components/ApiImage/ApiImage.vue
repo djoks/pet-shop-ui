dee320d6cdb947f3efc29fc6cbff5154ff6348cf
djoks <contact.djoks@gmail.com>
2023-09-08 05:07:51 +0000

    refactor: code cleanup.

--- components/ApiImage/ApiImage.vue
+++ components/ApiImage/ApiImage.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import type { ApiImageProps } from "./ApiImage.types"
-
 const { uuid } = defineProps<{ uuid: string }>();
 const src = `https://pet-shop.buckhill.com.hr/api/v1/file/${uuid}`;
 
